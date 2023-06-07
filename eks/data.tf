e48084d1f68c83e649f117e832e23f6c478a41e3
Tania Ponomarenko <tane4ka377@gmail.com>
2023-06-07 19:24:59 +0300

    created role for eks node group

--- eks/data.tf
+++ eks/data.tf
@@ -10,3 +10,16 @@ data "aws_iam_policy_document" "eks_cluster_role_assume_policy" {
     }
   }
 }
+
+data "aws_iam_policy_document" "eks_node_group_role_assume_policy" {
+  statement {
+    effect = "Allow"
+    actions = [
+      "sts:AssumeRole",
+    ]
+    principals {
+      type        = "Service"
+      identifiers = ["ec2.amazonaws.com"]
+    }
+  }
+}
--- eks/iam_roles.tf
+++ eks/iam_roles.tf
@@ -17,3 +17,9 @@ resource "aws_iam_policy_attachment" "eks_cluster_role_policy_attachment" {
   roles      = [aws_iam_role.eks_cluster_role.name]
   policy_arn = each.key
 }
+
+resource "aws_iam_role" "eks_node_group_role" {
+  name = "eks-node-group-role"
+
+  assume_role_policy = data.aws_iam_policy_document.eks_node_group_role_assume_policy.json
+}
