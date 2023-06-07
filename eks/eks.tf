53f88f8b03a136dcdf8f424733e70bd2ce9498c5
Anna Strukova <metiletan@gmail.com>
2023-06-07 20:03:19 +0200

    change worker node type

--- eks/eks.tf
+++ eks/eks.tf
@@ -44,6 +44,9 @@ resource "aws_eks_node_group" "main_node_group" {
   node_group_name = "main-node-group"
   node_role_arn   = aws_iam_role.eks_node_group_role.arn
   subnet_ids      = local.subnet_ids
+  instance_types = [
+    "t2.micro"
+  ]
 
   scaling_config {
     desired_size = 1
