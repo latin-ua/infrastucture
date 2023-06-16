618b072dbd4bd5f2e1e38c6ea0c0f533951a82b9
Anna Strukova <metiletan@gmail.com>
2023-06-16 15:46:54 +0200

    create aws budget for latin-ua, budget amount is in variables file

---
+++ common/budget.tf
@@ -0,0 +1,15 @@
+resource "aws_budgets_budget" "latin_ua_budget" {
+  name         = "latin-ua-budget"
+  budget_type  = "COST"
+  limit_amount = var.budget_amount
+  limit_unit   = "USD"
+  time_unit    = "MONTHLY"
+
+  notification {
+    comparison_operator        = "GREATER_THAN"
+    threshold                  = 50
+    threshold_type             = "PERCENTAGE"
+    notification_type          = "FORECASTED"
+    subscriber_email_addresses = [var.notification_email]
+  }
+}
