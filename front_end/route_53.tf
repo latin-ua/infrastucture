e366940a7e846a6d3d19d52395f9ed41a49a4e49
Tania Ponomarenko <tane4ka377@gmail.com>
2023-07-05 20:18:01 +0300

    create dns name with aws_route53

---
+++ front_end/route_53.tf
@@ -0,0 +1,18 @@
+locals {
+  domain_name = "www.latin.com.ua"
+}
+resource "aws_route53_zone" "primary" {
+  name = local.domain_name
+}
+
+resource "aws_route53_record" "front_end" {
+  zone_id = aws_route53_zone.primary.zone_id
+  name    = local.domain_name
+  type    = "A"
+
+  alias {
+    name                   = var.front_end_lb_dns_name
+    zone_id                = var.front_end_lb_zone_id
+    evaluate_target_health = true
+  }
+}
