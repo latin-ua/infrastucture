16b261d571a6ee428ec1060af3c087c451b58c25
Tania Ponomarenko <tane4ka377@gmail.com>
2023-07-07 21:05:03 +0300

    change zone in route_53 front_end

--- front_end/route_53.tf
+++ front_end/route_53.tf
@@ -1,18 +1,15 @@
 locals {
   domain_name = "www.latin.com.ua"
 }
-resource "aws_route53_zone" "primary" {
-  name = local.domain_name
-}
 
 resource "aws_route53_record" "front_end" {
-  zone_id = aws_route53_zone.primary.zone_id
+  zone_id = "Z015263031Q4FAY12JMRL"
   name    = local.domain_name
   type    = "A"
 
   alias {
     name                   = var.front_end_lb_dns_name
-    front_end_lb_zone_id   = var.front_end_lb_zone_id
+    zone_id                = var.front_end_lb_zone_id
     evaluate_target_health = true
   }
 }
