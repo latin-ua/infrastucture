48d5f4fdcb44b9c2b479045546089ea205865180
Anna Strukova <metiletan@gmail.com>
2023-08-07 19:15:06 +0200

    uncomment domain_validation

--- common/cdn.tf
+++ common/cdn.tf
@@ -81,18 +81,18 @@ resource "aws_acm_certificate" "cdn_certificate" {
   }
 }
 
-#resource "aws_route53_record" "domain_validation" {
-#  for_each = {
-#    for dvo in aws_acm_certificate.cdn_certificate.domain_validation_options : dvo.domain_name => {
-#      name   = dvo.resource_record_name
-#      record = dvo.resource_record_value
-#      type   = dvo.resource_record_type
-#    }
-#  }
-#
-#  zone_id = aws_route53_record.cdn_main.zone_id
-#  name    = each.value.name
-#  type    = each.value.type
-#  records = [each.value.record]
-#  ttl     = 60
-#}
+resource "aws_route53_record" "domain_validation" {
+  for_each = {
+    for dvo in aws_acm_certificate.cdn_certificate.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
+
+  zone_id = aws_route53_record.cdn_main.zone_id
+  name    = each.value.name
+  type    = each.value.type
+  records = [each.value.record]
+  ttl     = 60
+}
