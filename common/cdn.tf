6ab077e4fa27183ecac648669f9fca7892a37243
Anna Strukova <metiletan@gmail.com>
2023-08-07 19:20:32 +0200

    add ssl supported method

--- common/cdn.tf
+++ common/cdn.tf
@@ -46,6 +46,7 @@ resource "aws_cloudfront_distribution" "latin_ua_distribution" {
   viewer_certificate {
     cloudfront_default_certificate = false
     acm_certificate_arn            = aws_acm_certificate.cdn_certificate.arn
+    ssl_support_method             = "sni-only"
   }
 
 }
