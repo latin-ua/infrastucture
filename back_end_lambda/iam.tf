9a635fb6d8e2b23e176729046e28263646fbc2da
Anna Strukova <metiletan@gmail.com>
2023-09-14 23:11:10 +0200

    add provider for archive

--- back_end_lambda/iam.tf
+++ back_end_lambda/iam.tf
@@ -2,3 +2,14 @@ resource "aws_iam_role" "iam_role_for_lambda" {
   name               = "iam-role-for-lambda"
   assume_role_policy = data.aws_iam_policy_document.lambda_assume_role.json
 }
+
+resource "aws_iam_policy" "lambda_cloud_watch" {
+  name   = "aws-load-balancer-controller-additional-iam-policy"
+  policy = data.aws_iam_policy_document.lambda_cloud_watch.json
+}
+
+resource "aws_iam_policy_attachment" "lambda_cloud_watch_policy_attachment" {
+  name       = "lambda-cloud-watch-attachment"
+  roles      = [aws_iam_role.iam_role_for_lambda.name]
+  policy_arn = aws_iam_policy.lambda_cloud_watch.arn
+}
